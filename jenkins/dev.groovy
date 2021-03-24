9fa2bc05bb9f6e68054a166f9980b7227e907c15
yuenbin <yuenbin@inspur.com>
2021-03-24 16:27:08 +0800

    增加docker、jenkins快速集成部署

--- jenkins/dev.groovy
+++ jenkins/dev.groovy
@@ -1,9 +1,8 @@
      def label = "mydev-${UUID.randomUUID().toString()}"
     def img_version = '${img_version}'
-    def project_name = "springboot-sharding-dev"
+    def project_name = "smart-kettle"
     def img_name = project_name
     def package_name = "yaukie"
-    def git_address = "github.com/"+package_name+"/"+project_name+".git"
     def setting_name = "settings"
     def repo_url = "registry.cn-hangzhou.aliyuncs.com"
     def user_name = "yaukie@163.com"
@@ -19,7 +18,7 @@
                        extensions: [],
                        submoduleCfg: [],
                        userRemoteConfigs: [[credentialsId: '67383ec2-d738-4632-b4b8-36c70180efb0',
-                       url: 'http://open.inspur.com/yuenbin/springboot-sharding-dev.git']]]
+                       url: 'https://gitee.com/yaukie/x-smart-kettle-server.git']]]
              )
              echo '仓库源码拉取成功....'
          }
@@ -34,12 +33,12 @@
              echo '准备推送镜像....'
              dir("docker/"){
                  sh 'ls -al '
-                 sh 'rm -f  *.jar'
+                 sh 'rm -f  *.tar.gz'
                  echo project_name+'.jar删除成功,准备登录阿里云....'
              }
                sh 'docker login -u '+user_name+ ' -p '+pass_word+' registry.cn-hangzhou.aliyuncs.com'
              echo '阿里云镜像仓库登录成功....'
-             sh 'cp target/*.jar  docker/'
+             sh 'cp target/*.tar.gz  docker/'
              def img_url = repo_url + '/'+ package_name+'/'+project_name+':'+img_version
              sh 'cd  docker/ && docker build -t '+ img_url + ' .'
              sh 'docker push '+ img_url
